f49d8924e349a90df511e941c7d2591300a9febf
EC2 Default User <ec2-user@ip-172-31-24-28.ap-northeast-1.compute.internal>
2022-08-21 07:38:27 +0000

    [add]離席のためcommit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_one_attached :profile_imag
+
+
   #has_one_attached :image
 end
--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require_relative "application"
 Rails.application.initialize!
 
 #const webpack = require('webpack')
-# environment.plugins.prepend(
+#environment.plugins.prepend(
 #  'Provide',
 #  new webpack.ProvidePlugin({
 #    $: 'jquery/src/jquery',
