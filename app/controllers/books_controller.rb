c84b4ad7bcc236ec008bba7c70fcc37c008cfb0c
EC2 Default User <ec2-user@ip-172-31-24-28.ap-northeast-1.compute.internal>
2022-08-22 07:21:32 +0000

    [add]bookのnew,create,indexアクションの追記およびindexのviewページ作成(未完成)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,13 @@
 class BooksController < ApplicationController
  def new
+  @book = Book.new
  end
 
  def create
+  @book = Book.new(book_params)
+  @book.user_id = current_user.id
+  @book.save
+  redirect_to book_path(book.id)
  end
 
  def index
@@ -19,6 +24,12 @@ class BooksController < ApplicationController
  end
 
  def delete
+ end
+
+ private
+
+ def book_params
+  params.require(:book).permit(:titel, :opinion)
  end
 
 end
